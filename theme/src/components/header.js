ab197b21db2a0a9e8ebe4aecc0fd00dd5477d146
niklasmtj <22395665+niklasmtj@users.noreply.github.com>
2019-07-20 13:01:20 +0200

    Update header.js to GitHub theme link

--- theme/src/components/header.js
+++ theme/src/components/header.js
@@ -78,7 +78,7 @@ const Header = ({ siteTitle }) => (
       <p>
         <HomeLink to="/">{siteTitle}</HomeLink>
         <NavLink to="/blog">Blog</NavLink>
-        <GitHubLink href="https://github.com/niklasmtj/gatsby-theme-julia">GitHub</GitHubLink>
+        <GitHubLink href="https://github.com/niklasmtj/gatsby-theme-julia/tree/master/theme">GitHub</GitHubLink>
       </p>
     </Content>
   </SiteHeader>
